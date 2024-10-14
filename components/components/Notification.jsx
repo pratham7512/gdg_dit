d39e5647c01049ebbde28c5498a982c5178a673b
suhelali14 <106341120+suhelali14@users.noreply.github.com>
2024-10-14 21:00:10 +0530

    Added our old frontend

---
+++ components/components/Notification.jsx
@@ -0,0 +1,29 @@
+import { notification1 } from "../assets";
+import { notificationImages } from "../../constants";
+import Image from "next/image";
+
+const Notification = ({ className, title }) => {
+  return (
+    <div
+      className={`${
+        className || ""
+      } flex items-center p-4 pr-6 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl gap-5`}
+    >
+      <Image
+        src={notification1}
+        width={62}
+        height={62}
+        alt="image"
+        className="rounded-xl"
+      />
+
+      <div className="flex-1">
+        <h6 className="mb-1 font-semibold text-base">{title}</h6>
+
+        
+      </div>
+    </div>
+  );
+};
+
+export default Notification;
