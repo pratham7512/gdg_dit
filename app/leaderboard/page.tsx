73511d804e47fa36b0fba97259490075b02743c8
Prathamesh Desai <prathameshdesai679@gmail.com>
2024-11-04 03:19:53 +0530

    loading-animationa-fix

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -7,12 +7,30 @@ import ButtonGradient from "@/components/assets/svg/ButtonGradient";
 import Leaderboard from '@/components/components/Leaderboard';
 import { useSession } from "next-auth/react";
 import { redirect } from 'next/navigation';
+import { Skeleton } from '@/components/ui/skeleton';
+import { TableCell, TableRow } from '@/components/ui/table';
 
 
 const Ranking =() => {
   const {status} =  useSession();
   if(status==="loading"){
-    return <p>Loading....</p>
+    return(<>
+    {[...Array(10)].map((_, index) => (
+      <TableRow key={index} className="border-b border-gray-800 animate-pulse">
+        <TableCell><Skeleton className="h-6 w-8" /></TableCell>
+        <TableCell>
+          <div className="flex items-center space-x-3">
+            <Skeleton className="h-12 w-12 rounded-full" />
+            <Skeleton className="h-6 w-32" />
+          </div>
+        </TableCell>
+        <TableCell><Skeleton className="h-6 w-12 ml-auto" /></TableCell>
+        <TableCell><Skeleton className="h-6 w-12 ml-auto" /></TableCell>
+        <TableCell><Skeleton className="h-6 w-12 ml-auto" /></TableCell>
+        <TableCell><Skeleton className="h-6 w-16 ml-auto" /></TableCell>
+      </TableRow>
+    ))}
+  </>)
   }
   if (status==='unauthenticated') {
     redirect('/');
