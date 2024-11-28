c277f23dda06dbc1acd3764750c637b399c1b9e7
suhelali14 <106341120+suhelali14@users.noreply.github.com>
2024-11-28 22:19:24 +0530

    solve some bugs

--- app/page.tsx
+++ app/page.tsx
@@ -26,29 +26,25 @@ export default function Home() {
     })();
   }, []);
 
-  // Compare dates using getTime() for accurate comparison
-  if (0) {
-    return <ComingSoon />; // Show ComingSoon if before 5:30 PM
-  } else {
-    return (
-      <>
-        <AnimatePresence mode='wait'>
-          {isLoading && <Preloader />}
-        </AnimatePresence>
-        <AnimatePresence mode='wait'>
-          {!isLoading && <Chatbot />}
-        </AnimatePresence>
-        <div className="pt-[4rem] lg:pt-[2.8rem] overflow-hidden bg-black bg-grid-white/[0.090]">
-          <Header />
-          <Hero />
-          <Benefits />
-          <Collaboration />
-          <Roadmap />
-          <Community />
-          <Footer />
-        </div>
-        <ButtonGradient />
-      </>
-    );
-  }
+
+  return (
+    <>
+      <AnimatePresence mode='wait'>
+        {isLoading && <Preloader />}
+      </AnimatePresence>
+      <AnimatePresence mode='wait'>
+        {!isLoading && <Chatbot />}
+      </AnimatePresence>
+      <div className="pt-[4rem] lg:pt-[2.8rem] overflow-hidden bg-black bg-grid-white/[0.090]">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Roadmap />
+        <Community />
+        <Footer />
+      </div>
+      <ButtonGradient />
+    </>
+  );
 }
