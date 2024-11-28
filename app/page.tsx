8081ea0bb351e45fbff42e7031ca01b8c59a7238
Prathamesh Desai <115919176+pratham7512@users.noreply.github.com>
2024-11-28 17:12:32 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,8 @@
-"use client"
-import { AnimatePresence } from 'framer-motion';
-import { useEffect, useState } from "react";
-import Preloader  from "@/components/Preloader";
-import Chatbot  from "@/components/Chatbot";
-import Benefits  from "@/components/components/Benefits";
-import Header  from "@/components/components/Header";
-import Hero  from "@/components/components/Hero";
-import Collaboration  from "@/components/components/Collaboration";
-import Roadmap  from "@/components/components/Roadmap";
-import Footer  from "@/components/components/Footer";
-import ButtonGradient from "../components/assets/svg/ButtonGradient";
-import Community from '@/components/components/Community';
-import ComingSoon from '@/components/components/CommingSoon';
-
-
 export default function Home() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const currentDate = new Date();
+  const targetDate = new Date('2025-11-28T17:00:00+05:30'); // 5:30 PM IST on 28-11-2025
+
   useEffect(() => {
     (async () => {
       // const LocomotiveScroll = (await import('locomotive-scroll')).default;
@@ -28,26 +15,30 @@ export default function Home() {
       }, 2000);
     })();
   }, []);
-  return (
-    < >
-      <AnimatePresence mode='wait'>
+
+  // Check if current date is before the target date
+  if (currentDate < targetDate) {
+    return <ComingSoon />; // Show ComingSoon if before 5:30 PM
+  } else {
+    return (
+      <>
+        <AnimatePresence mode='wait'>
           {isLoading && <Preloader />}
-      </AnimatePresence>
-      <AnimatePresence mode='wait'>
+        </AnimatePresence>
+        <AnimatePresence mode='wait'>
           {!isLoading && <Chatbot />}
-      </AnimatePresence>
-      <div className="pt-[4rem] lg:pt-[2.8rem] overflow-hidden bg-black bg-grid-white/[0.090]">
-        <Header />
-        <Hero />
-        <Benefits/>
-        <Collaboration />
-        <Roadmap />
-        <Community/>
-        <Footer />
-      </div>
-      <ButtonGradient />
-          
-      
-    </>
-  );
+        </AnimatePresence>
+        <div className="pt-[4rem] lg:pt-[2.8rem] overflow-hidden bg-black bg-grid-white/[0.090]">
+          <Header />
+          <Hero />
+          <Benefits />
+          <Collaboration />
+          <Roadmap />
+          <Community />
+          <Footer />
+        </div>
+        <ButtonGradient />
+      </>
+    );
+  }
 }
