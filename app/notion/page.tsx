2f8a32208b261838f9a92211af2c6108df685299
Prathamesh Desai <prathameshdesai679@gmail.com>
2024-12-02 17:17:30 +0530

    yNo changes detected in the current diff.

--- app/notion/page.tsx
+++ app/notion/page.tsx
@@ -1,15 +1,30 @@
 import { NotionPage } from "@/components/components/NotionPage";
-import {notion} from "@/lib/notion"
+import { notion } from "@/lib/notion";
 
-const rootPageId = "98d8acfc9a0c4b98b94d68324d219b97";
+export const fetchRoadmap = async (id: string): Promise<string> => {
+    const response = await fetch(`/api/roadmap?id=${id}`);
+    const data = await response.json();
+    return data.notionLink ;
+};
 
-async function getData(rootPageId:string) {
-    return await notion.getPage(rootPageId);
-  }
-  export default async function Home() {
-    const data = await getData(rootPageId);
-  
-    return (
+// Default root page ID
+const defaultRootPageId = "6b6c2a9f1282499aba4782b88bf7e204";
+
+// Fetch the Notion page data based on the provided rootPageId
+async function getData(rootPageId: string) {
+  return await notion.getPage(rootPageId);
+}
+
+export default async function Home() {
+  const notionLink = await fetchRoadmap("95fc1a80-eae1-4c0c-ae2f-85c5e91ab16a");
+
+  const rootPageId = notionLink || defaultRootPageId;
+  const data = await getData(rootPageId);
+
+  return (
+    <>
+      <div> This is the fetched notion link: {notionLink} </div>
       <NotionPage recordMap={data} rootPageId={rootPageId} />
-    );
-  }
+    </>
+  );
+}
