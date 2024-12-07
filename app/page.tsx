55fce47ac30eef0f31ef307265825a039fbfd4fb
Prathamesh Desai <prathameshdesai679@gmail.com>
2024-12-08 00:36:38 +0530

    fixing

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import { AnimatePresence } from 'framer-motion';
 import { useEffect, useState } from "react";
 import Preloader from "@/components/Preloader";
 import Chatbot from "@/components/Chatbot";
-import Benefits from "@/components/components/Benefits";
-import Header from "@/components/components/Header";
-import Hero from "@/components/components/Hero";
-import Collaboration from "@/components/components/Collaboration";
-import Roadmap from "@/components/components/Roadmap";
-import Footer from "@/components/components/Footer";
+// import Benefits from "@/components/components/Benefits";
+// import Header from "@/components/components/Header";
+// import Hero from "@/components/components/Hero";
+// import Collaboration from "@/components/components/Collaboration";
+// import Roadmap from "@/components/components/Roadmap";
+// import Footer from "@/components/components/Footer";
 import ButtonGradient from "../components/assets/svg/ButtonGradient";
-import Community from '@/components/components/Community';
+// import Community from '@/components/components/Community';
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
@@ -37,13 +37,13 @@ export default function Home() {
         {!isLoading && <Chatbot />}
       </AnimatePresence>
       <div className="pt-[4rem] lg:pt-[2.8rem] overflow-hidden bg-black bg-grid-white/[0.090]">
-        <Header />
+        {/* <Header />
         <Hero />
         <Benefits />
         <Collaboration />
         <Roadmap />
         <Community />
-        <Footer />
+        <Footer /> */}
       </div>
       <ButtonGradient />
     </>
